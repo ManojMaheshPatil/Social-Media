7f52c780c6337829ffa93bc4fb510236bc03ead2
ManojMaheshPatil <mprdjcr7@gmail.com>
2023-01-21 15:24:37 +0530

    First commit

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,41 @@
+import { Box , useMediaQuery } from "@mui/material";
+import NavBar from "../navbar";
+import { useSelector } from "react-redux";
+import UserWidget from "../widgets/UserWidget";
+import MyPostWidget from "../widgets/MyPostWidget";
+import AdvertWidget from "../widgets/AdvertWidget";
+import FriendListWidget from "../widgets/FriendListWidget";
+
+const HomePage = () => {
+    const isNonMobileScreens = useMediaQuery("(min-wdith: 1000px)");
+    const { _id , picturePath } = useSelector((state)=>state.user);
+    return(
+        <Box>
+            <NavBar/>
+            <Box
+                width = "100%"
+                padding = "2rem 6%"
+                display = {isNonMobileScreens?"flex":"block"}
+                gap = "0.rem"
+                justifyContent = "space-between"
+            >
+                <Box flexBasis={isNonMobileScreens?"26%":undefined}>
+                    <UserWidget userId={_id} picturePath={picturePath} />
+                </Box>
+                <Box 
+                    flexBasis={isNonMobileScreens ? "42%": undefined}
+                    mt={isNonMobileScreens?undefined : "2rem"}
+                >
+                    <MyPostWidget picturePath={picturePath}/>
+                </Box>
+                {isNonMobileScreens && <Box flexBasis="26%">
+                    <AdvertWidget/>
+                    <Box m="2rem 0"/>
+                    <FriendListWidget userId={_id}/>
+                    </Box>}
+            </Box>
+        </Box>
+    )
+}
+
+export default HomePage;
